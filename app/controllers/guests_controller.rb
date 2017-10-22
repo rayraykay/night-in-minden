8d1a1c2ac1f3bc8bad073ff4a1c7ce46a088a8af
rayraykay <raymond.d.kim@outlook.com>
2017-10-22 17:14:54 -0400

    fixed edit path

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,5 +1,5 @@
 class GuestsController < ApplicationController
-	skip_before_action :require_login, :only => [:new, :create]
+	skip_before_action :require_login, :only => [:new, :create, :edit]
 
 	# for admin side
 	def index
