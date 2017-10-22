64403f10b93f13a8b5b9db45381a0a57ae5e3ba9
rayraykay <raymond.d.kim@outlook.com>
2017-10-22 04:41:08 -0400

    added table defaults

--- db/migrate/20171022083049_create_tables.rb
+++ db/migrate/20171022083049_create_tables.rb
@@ -1,7 +1,7 @@
 class CreateTables < ActiveRecord::Migration[5.1]
   def change
     create_table :tables do |t|
-      t.integer :max_capacity
+      t.integer :max_capacity, default: 10
 
       t.timestamps
     end
