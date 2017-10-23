11b349062d921cc4ccb2fdc71c4f12ab35eb1311
rayraykay <raymond.d.kim@outlook.com>
2017-10-22 20:19:26 -0400

    fixed sign up error, need to refactor later, and added prof tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
 	NUM_OF_TICKETS = 300
+	PROFESSOR_TABLES = [3, 4, 5]
 
 	def ticket_count
 		return NUM_OF_TICKETS
+	end
+	
+	def is_prof_table?(table_id)
+		return PROFESSOR_TABLES.include?(table_id)
 	end
 end
